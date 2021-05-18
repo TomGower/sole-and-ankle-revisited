6b36f590920e5df28fc4cc1f134f87bf5a4d94e9
TomGower <tom.gower@gmail.com>
2021-05-17 19:47:32 -0500

    Module 5: Workshop: Mobile Menu continued

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,12 +3,11 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
-import { X } from 'react-feather';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   // if (!isOpen) {
@@ -22,28 +21,27 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
     >
       <Content aria-label={'Menu'}>
         <Header>
-          <Title>Menu</Title>
           <CloseButton onClick={onDismiss}>
-            <X />
+            <Icon id={'close'} color={COLORS.gray[900]} size={'1.5rem'} strokeWidth={1} />
             <VisuallyHidden>
               Dismiss modal
             </VisuallyHidden>
           </CloseButton>
         </Header>
         <div>
-          <nav>
+          <Nav>
             <a href="/sale">Sale</a>
             <a href="/new">New&nbsp;Releases</a>
             <a href="/men">Men</a>
             <a href="/women">Women</a>
             <a href="/kids">Kids</a>
             <a href="/collections">Collections</a>
-          </nav>
-          <footer>
+          </Nav>
+          <Footer>
             <a href="/terms">Terms and Conditions</a>
             <a href="/privacy">Privacy Policy</a>
             <a href="/contact">Contact Us</a>
-          </footer>
+          </Footer>
         </div>
       </Content>
     </Overlay>
@@ -51,18 +49,54 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
 };
 
 const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: ${COLORS.gray[700]};
 `;
 
 const Content = styled(DialogContent)`
+  text-transform: capitalize;
+  position: relative;
+  background: ${COLORS.white};
+  width: 75%;
 `;
 
 const Header = styled.header`
 `;
 
 const CloseButton = styled.button`
+  position: absolute;
+  top: 2rem;
+  right: 1.375rem;
+  border: none;
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Title = styled.h2`
+const Nav = styled.nav`
+  color: ${COLORS.gray[900]};
+  font-size: 1.125rem;
+  margin-left: 2rem;
+  margin-top: auto;
+  margin-bottom: auto;
+`;
+
+const Footer = styled.footer`
+  color: ${COLORS.gray[700]};
+  font-size: 0.875rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.875rem;
+  position: absolute;
+  bottom: 2rem;
+  left: 2rem;
 `;
 
 export default MobileMenu;
